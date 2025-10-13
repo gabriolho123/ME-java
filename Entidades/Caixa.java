b7711784cd867b6fe0973e208fddfae95f267b6b
Gabriel Marafante <BIBIMARAFANTE@GMAIL.COM>
2025-10-13 03:47:32 -0300

    Update and rename Caixa.java to Entidades/Caixa.java

--- Entidades/Caixa.java
+++ Entidades/Caixa.java
@@ -1,13 +1,13 @@
 package modelo.entidades;
 
-// O erro no super(matricula, nome) deve sumir após corrigir Usuario.java
+// O erro no super(matricula, nome) deve sumir apÃ³s corrigir Usuario.java
 public class Caixa extends Usuario {
 
     public Caixa(String matricula, String nome) { 
         super(matricula, nome); 
     }
 
-    // Método de simulação de pagamento
+    // MÃ©todo de simulaÃ§Ã£o de pagamento
     public boolean registrarPagamento(Boleto boleto) {
         if (boleto != null) {
             boleto.registrarPagamento(new java.util.Date()); 
